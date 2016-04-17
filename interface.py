bc4bc4b797929210bc3b7fbfebfef9837731e534
Tyler <tylercash6@gmail.com>
2016-04-18 01:14:26 +1000

    Passing test_add_like

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 '''
 @author:
 '''
+import datetime
 
 
 def get_likes(cur):
@@ -22,7 +23,6 @@ def list_images(db, n, usernick=None):
     list_of_images = []
 
     for row in cur.fetchall():
-
         # TODO add list_comments to dictionary
         image = {'filename': row[0], 'timestamp': row[1], 'user': row[2], 'likes': count_likes(db, row[0])}
         list_of_images.append(image)
@@ -36,11 +36,31 @@ def add_image(db, filename, usernick):
 
 def add_like(db, filename, usernick=None):
     """Increment the like count for this image"""
+    cur = db.cursor()
+
+    if usernick is None:
+        # Ensures file exists
+        cur.execute("SELECT * FROM images WHERE filename='" + filename + "';")
+        if len(cur.fetchall()) < 1:
+            return
+
+        cur.execute("INSERT INTO likes VALUES('" + filename + "', NULL)")
+    else:
+        # Ensures user exists
+        cur.execute("SELECT * FROM users WHERE `nick`='" + usernick + "'")
+        if len(cur.fetchall()) < 1:
+            return
+        # Ensures file exists
+        cur.execute("SELECT * FROM images WHERE `filename`='" + filename + "'")
+        if len(cur.fetchall()) < 1:
+            return
+
+            # TODO possibly add check to see if user has liked picture
+        cur.execute("INSERT INTO likes VALUES('" + filename + "', '" + usernick + "');")
 
 
 def count_likes(db, filename):
     """Count the number of likes for this filename"""
     cur = db.cursor()
-    cur.execute('SELECT `usernick` FROM likes WHERE `filename`=\'' + filename + "';")
+    cur.execute("SELECT `usernick` FROM likes WHERE `filename`='" + filename + "';")
     return len(cur.fetchall())
-
