8169e312df56711383fcf5d6bc0a0f671b3ae2f5
Tyler <tylercash6@gmail.com>
2016-04-19 03:15:44 +1000

    Reverted app name change

--- main.py
+++ main.py
@@ -6,39 +6,40 @@ from bottle import Bottle, template, debug
 
 COOKIE_NAME = 'sessionid'
 
-app = Bottle()
+application = Bottle()
 
-@app.route('/')
+
+@application.route('/')
 def home_page():
     return template('index', title="Home")
 
 
-@app.route('/about')
+@application.route('/about')
 def about_page():
     return template('about', title="About")
 
 
-@app.route('/profile')
+@application.route('/profile')
 def about_page():
     return template('profile', title="Profile")
 
 
 # Serves static files
-@app.route('/static/images/<filename:path>')
+@application.route('/static/images/<filename:path>')
 def serve_images(filename):
     return bottle.static_file(filename, root='static/images')
 
 
-@app.route('/static/js/<filename:path>')
+@application.route('/static/js/<filename:path>')
 def serve_javascripts(filename):
     return bottle.static_file(filename, root='static/js')
 
 
-@app.route('/static/css/<filename:path>')
+@application.route('/static/css/<filename:path>')
 def serve_javascripts(filename):
     return bottle.static_file(filename, root='static/css')
 
 
 if __name__ == '__main__':
     debug()
-    app.run()
+    application.run()
