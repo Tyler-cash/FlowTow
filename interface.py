2b93cc27d099d72e379e9590c8c4ab9beb1f31d4
Tyler Cash <TylerCash6@gmail.com>
2016-05-10 15:23:58 +1000

    Sanitized database queries. Very weird behaviour though! Some queries required a tuple while others required just a plain string.

--- interface.py
+++ interface.py
@@ -10,11 +10,12 @@ def list_images(db, n, usernick=None):
     The 'comments' value will be a list of comments associated with this image (as returned by list_comments).
     If usernick is given, then only images belonging to that user are returned."""
     cur = db.cursor()
-
     if usernick is None:
         cur.execute('SELECT * FROM images ORDER BY timestamp DESC;')
     else:
-        cur.execute('SELECT * FROM images WHERE usernick="' + usernick + '" ORDER BY timestamp DESC;')
+        # No idea why this needs to be a tuple but you can't just insert a string into the query
+        nick = (usernick,)
+        cur.execute('SELECT * FROM images WHERE usernick = ? ORDER BY timestamp DESC;', (nick))
     db.conn.commit()
     list_of_images = []
     i = 0;
@@ -34,43 +35,45 @@ def list_images(db, n, usernick=None):
 def add_image(db, filename, usernick):
     """Add this image to the database for the given user"""
     cur = db.cursor()
-    cur.execute(
-        "INSERT INTO images VALUES('" + filename + "','" + str(datetime.datetime.now()) + "', '" + usernick + "');")
+    cur.execute("INSERT INTO images VALUES(?,?,?);", (filename, str(datetime.datetime.now()), usernick))
     db.conn.commit()
+
 
 def add_like(db, filename, usernick=None):
     """Increment the like count for this image"""
     cur = db.cursor()
+    file = (filename,)
 
     if usernick is None:
-        # Ensures file exists
-        cur.execute("SELECT * FROM images WHERE filename='" + filename + "';")
+        cur.execute("SELECT * FROM images WHERE filename=?;", file)
         db.conn.commit()
         if len(cur.fetchall()) < 1:
             return
 
-        cur.execute("INSERT INTO likes VALUES('" + filename + "', NULL)")
+        cur.execute("INSERT INTO likes VALUES(?, NULL)", file)
         db.conn.commit()
     else:
+        nick = (usernick,)
         # Ensures user exists
-        cur.execute("SELECT * FROM users WHERE `nick`='" + usernick + "'")
+        cur.execute("SELECT * FROM users WHERE nick=?", nick)
         db.conn.commit()
         if len(cur.fetchall()) < 1:
             return
         # Ensures file exists
-        cur.execute("SELECT * FROM images WHERE `filename`='" + filename + "'")
+        cur.execute("SELECT * FROM images WHERE filename=?", (file))
         db.conn.commit()
         if len(cur.fetchall()) < 1:
             return
 
             # TODO possibly add check to see if user has liked picture
-        cur.execute("INSERT INTO likes VALUES('" + filename + "', '" + usernick + "');")
+        cur.execute("INSERT INTO likes VALUES(?,?);", (filename, usernick))
         db.conn.commit()
 
 
 def count_likes(db, filename):
     """Count the number of likes for this filename"""
     cur = db.cursor()
-    cur.execute("SELECT `usernick` FROM likes WHERE `filename`='" + filename + "';")
+    file = (filename,)
+    cur.execute("SELECT `usernick` FROM likes WHERE filename=?;", file)
     db.conn.commit()
     return len(cur.fetchall())
