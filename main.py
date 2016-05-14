79dff6c73b2bfc6c5dcc1279206a7ffbe082967b
Tyler Cash <TylerCash6@gmail.com>
2016-05-14 13:56:41 +1000

    Gave each method a docstring to explain it's purpose

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ COOKIE_NAME = 'sessionid'
 application = Bottle()
 
 
-# Returns hidden if user is not logged in and if user is logged in returns ""
-def hidden_my_images():
+def hide_my_images():
+    """
+    Returns hidden if user is not logged in and if user is logged in returns ""
+    """
     db = instanceOfDatabase.db
     users_status = ""
     # If user is not logged in hide my images
@@ -28,59 +30,91 @@ def hidden_my_images():
 
 @application.route('/')
 def home_page():
-    return template('index', title="Home", usernick="None", loggedIn=hidden_my_images())
+    """
+    Returns the homepage
+    """
+    return template('index', title="Home", usernick="None", loggedIn=hide_my_images())
 
 
 @application.route('/about')
 def about_page():
-    return template('about', title="About", loggedIn=hidden_my_images())
+    """
+    Returns the about flowtow page
+    """
+    return template('about', title="About", loggedIn=hide_my_images())
 
 
 @application.route('/my')
-def about_page():
+def my_profile_page():
+    """
+    If the user isn't logged in they will be redirected to
+    the homepage else this will return a page of all the user's
+    uploaded images.
+    """
     db = instanceOfDatabase.db
     if users.session_user(db) is not None:
-        return template('my', title="My Profile", loggedIn=hidden_my_images())
+        return template('my', title="My Profile", loggedIn=hide_my_images())
     else:
         return bottle.redirect('/', 303)
 
 
 @application.route('/login')
 def login_page():
-    return template('login', title="Login", errors="hidden", loggedIn=hidden_my_images())
+    """
+    Returns the login page
+    """
+    return template('login', title="Login", errors="hidden", loggedIn=hide_my_images())
 
 
 @application.route('/login-failed')
 def login_failed():
-    return template('login', title="Login", errors="", loggedIn=hidden_my_images())
+    """
+    Returns the login page. This is shown when a user's
+    submitted credentials can't be found in the db.
+    """
+    return template('login', title="Login", errors="", loggedIn=hide_my_images())
 
 
 @application.route('/comments/<filename>')
-def login_failed(filename):
+def comments_page(filename):
+    """
+    Returns a page with the discussions of a particular image
+    """
     if "jpg" in filename:
         db = instanceOfDatabase.db
         image = interface.get_image(db, filename)
         bottle.response.set_cookie('filename', filename)
         filename = filename.replace(".jpg", "")
+        # Redirects to url without filetype in url so that it's prettier
         return bottle.redirect('/comments/' + filename)
     else:
+        # If filename cookie not set then the user is reloading the page and
+        # therefore the comments page won't load.
         test = bottle.request.get_cookie('filename')
         if test is None:
             return bottle.redirect('/comments/' + filename + ".jpg")
-        return template('comment', title="Comments", loggedIn=hidden_my_images())
+
+        return template('comment', title="Comments", loggedIn=hide_my_images())
 
 
+# Post method handlers
 @application.post('/logout')
 def logout_user():
+    """
+    This method logs a user out of the site.
+    """
     db = instanceOfDatabase.db
     user_nick = users.session_user(db)
     users.delete_session(db, user_nick)
     return bottle.redirect('/', 303)
 
 
-# Post method handlers
 @application.post('/like')
 def like_image():
+    """
+    This method deals with the logic related to liking
+    an image.
+    """
     db = instanceOfDatabase.db
     liked_picture = request.forms.get('filename')
     if liked_picture is not None:
@@ -91,6 +125,10 @@ def like_image():
 
 @application.post('/login')
 def login_user():
+    """
+    This method deals with logging a user in or alerting
+    the user their credentials can't be found in the db.
+    """
     db = instanceOfDatabase.db
     username = request.forms.get('nick')
     password = request.forms.get('password')
@@ -107,6 +145,9 @@ def login_user():
 
 @application.post('/upload')
 def upload_file():
+    """
+    This method uploads files to the server.
+    """
     db = instanceOfDatabase.db
     user = users.session_user(db)
     if user is None:
@@ -130,20 +171,28 @@ def upload_file():
 # Serves static files
 @application.route('/static/images/<filename:path>')
 def serve_images(filename):
+    """
+    This method serves all the relevant images.
+    """
     return bottle.static_file(filename, root='static/images')
 
 
 @application.route('/static/js/<filename:path>')
 def serve_javascripts(filename):
+    """
+    This method serves all the relevant javascript.
+    """
     return bottle.static_file(filename, root='static/js')
 
 
 @application.route('/static/css/<filename:path>')
-def serve_javascripts(filename):
+def serve_css_sheets(filename):
+    """
+    This method serves all the relevant style sheets.
+    """
     return bottle.static_file(filename, root='static/css')
 
 
 if __name__ == '__main__':
     debug()
     application.run(host='192.168.1.103')
-    instanceOfDatabase.reset_database()
